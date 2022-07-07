8d01a4e9661cc677edf28f1099f0b5900bd57f09
kho903 <gmldnr2222@naver.com>
2022-07-08 01:11:23 +0900

    feat: 코틀린 기초 / 함수

---
+++ src/main/kotlin/Message.java
@@ -0,0 +1,16 @@
+public class Message {
+
+	public void greeting(String message) {
+		if (message == null || message.length() == 0) {
+			System.out.println("안녕하세요!!");
+			return;
+		}
+		System.out.println(message);
+	}
+
+	public static void main(String[] args) {
+		Message message = new Message();
+		message.greeting(null);
+		message.greeting("Hi!!!");
+	}
+}
