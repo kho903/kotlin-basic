151a89a72b881fbbea869e823c14cf9f7ca3d17c
kho903 <gmldnr2222@naver.com>
2022-07-13 17:59:22 +0900

    feat: 코틀린 기초 / 클래스와 프로퍼티

---
+++ src/main/kotlin/06_ClassProperty.kt
@@ -0,0 +1,50 @@
+//class Coffee constructor(val name: String) {
+//
+//}
+
+// constructor 생략 가능
+class Coffee(val name: String) {
+
+}
+
+class Coffee2(
+    val name: String,
+    val price: Int,
+    val brand: String,
+    ) {
+
+}
+
+class Coffee3(
+    var name: String = "",
+    var price: Int = 0,
+    var iced : Boolean = false,
+) {
+
+    val brand: String
+        get() = "스타벅스"
+
+    var quantity : Int = 0
+        set(value) {
+            if (value > 0) { // 수량이 0 보다 큰 경우에만 할당
+                field = value // field : 식별자
+//                quantity = value // 직접 할당할 경우 StackOverflow
+            }
+        }
+}
+
+class EmptyClass
+
+fun main() {
+
+    val coffee = Coffee3();
+    coffee.name = "아이스 아메리카노"
+    coffee.price = 2000
+    coffee.quantity = 1
+    coffee.iced = true
+
+    if (coffee.iced) {
+        println("아이스 커피")
+    }
+    println("${coffee.brand} ${coffee.name} 가격은 ${coffee.price} 수량은 ${coffee.quantity}")
+}
